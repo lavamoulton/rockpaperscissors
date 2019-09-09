bc00c2944d33555619ea086a69247e7d4dbf44f0
lavamoulton <danielzmoulton@gmail.com>
2019-09-09 12:36:16 -0400

    add simple UI

---
+++ rps.js
@@ -0,0 +1,96 @@
+function computerPlay() {
+    let rand = Math.floor(Math.random() * 3);
+    if (rand == 0) {
+        return "Rock";
+    } else if (rand == 1) {
+        return "Paper";
+    } else if (rand == 2) {
+        return "Scissors"
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    player = playerSelection.toUpperCase();
+    
+    if (player == "ROCK") {
+        switch(computerSelection) {
+            case "Rock":
+                return "You tied!";
+                break;
+            case "Paper":
+                return "You lose!";
+                break;
+            case "Scissors":
+                return "You win!";
+                break;
+        }
+    } else if (player == "PAPER") {
+        switch(computerSelection) {
+            case "Rock":
+                return "You tied!";
+                break;
+            case "Paper":
+                return "You lose!";
+                break;
+            case "Scissors":
+                return "You win!";
+                break;
+        }
+    } else if (player == "SCISSORS") {
+        switch(computerSelection) {
+            case "Rock":
+                return "You tied!";
+                break;
+            case "Paper":
+                return "You lose!";
+                break;
+            case "Scissors":
+                return "You win!";
+                break;
+        }
+    }
+}
+
+// add event listeners for all buttons
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+
+    button.addEventListener('click', (e) => {
+        game(button.id);
+    });
+});
+
+function game(playerBtn) {
+    
+    //assign player selection based on the button pressed
+    if (playerBtn = "rockbtn") {
+        playerSelection = "rock";
+    } else if (playerBtn = "paperbtn") {
+        playerSelection = "paper";
+    } else if (playerBtn = "scissorsbtn") {
+        playerSelection = "scissors";
+    }
+    
+    result = playRound(playerSelection, computerPlay());
+
+    alert(result);
+    
+    /*score = 0;
+    for (var i=0; i<5; i++) {
+        var playerSelection = prompt("What would you like to play?");
+        result = playRound(playerSelection, computerPlay());
+        console.log(result);
+        if (result == "You win!") {
+            score++;
+            //console.log(score);
+        }
+    }
+
+    if (score > 2) {
+        console.log("Congratulations! You won the game!");
+    } else {
+        console.log("You didn't win...");
+    } */
+}
+
+game();
